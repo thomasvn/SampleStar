7cb37266ffbdd7753a85f3de2caa16e4dc0897da
thomasvn <thomasnguyen96@gmail.com>
2022-07-02 18:49:05 -0700

    Updated application database directory for ElasticFileSystem

--- app/application.py
+++ app/application.py
@@ -10,15 +10,12 @@ import datetime
 # SETUP
 ################################################################################
 
-# Create the db directory if it doesn't exist
-if not os.path.exists('./db/'):
-    os.mkdir('./db')
+DB_DIR = './db'  # local deploy
+DB_DIR = '/efs'  # aws deploy
 
-# If the req2seq dictionary exists in the flat-file database, load it into memory
-req2seq = {}
-if os.path.exists('./db/requestId_to_sequenceId.json'):
-    with open('./db/requestId_to_sequenceId.json', 'r') as f:
-        req2seq = json.load(f)
+# Create the db directory if it doesn't exist
+if not os.path.exists(DB_DIR):
+    os.mkdir(DB_DIR)
 
 application = flask.Flask(__name__)
 
@@ -37,24 +34,33 @@ def randomFloat():
     return random.random()
 
 def genSeqId():
-    """Generate a pseudo-random id to associate with this generated sequence.
-    """
+    """Generate a pseudo-random id to associate with this generated sequence."""
+
     x = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
     seq_id = 'seq_' + x
 
     # If this seq_id already exists in db, try generating another seq_id
-    if os.path.exists('./db/' + seq_id + '.json'):
+    if os.path.exists(DB_DIR + '/' + seq_id + '.json'):
         return(genSeqId())
 
     return seq_id
 
+def getReq2Seq():
+    """Retrieve the mapping of request ids to sequence ids."""
+
+    if not os.path.exists(DB_DIR + '/requestId_to_sequenceId.json'):
+        return {}
+
+    with open(DB_DIR + '/requestId_to_sequenceId.json', 'r') as f:
+        return(json.load(f))
+        
 ################################################################################
 # ROUTES
 ################################################################################
 
 @application.route('/')
 def hello():
-    return 'Welcome to SampleStar!'
+    return 'Welcome to ⭐️ 🧪 SampleStar!'
 
 @application.route('/api/randomSequence', methods=['POST'])
 def randomSequence():
@@ -110,6 +116,7 @@ def randomSequence():
         return 'tag is not an identifier (alphanumerics and _)', 400
 
     # If the requestId exists, return the sequenceId to the user
+    req2seq = getReq2Seq()
     if requestId in req2seq:
         response['sequenceId'] = req2seq[requestId]
         return response
@@ -125,13 +132,14 @@ def randomSequence():
     response['sequenceId'] = sequenceId = genSeqId()
 
     # Update our req2seq lookup dict
+    req2seq = getReq2Seq()
     req2seq[requestId] = sequenceId
 
     # Write output to database
     # TODO: What if the program crashes right here? Roll back?
-    with open('./db/requestId_to_sequenceId.json', 'w') as f:
+    with open(DB_DIR + '/requestId_to_sequenceId.json', 'w') as f:
         json.dump(req2seq, f)
-    with open('./db/' + sequenceId + '.json', 'w') as f:
+    with open(DB_DIR + '/' + sequenceId + '.json', 'w') as f:
         json.dump(response, f)
 
     # Send response to user
@@ -182,17 +190,18 @@ def retrieveSequence():
         return 'sequenceId is not an identifier (alphanumerics and _)', 400
 
     # sequenceId does not exist in the database
-    if not os.path.exists('./db/' + sequenceId + '.json'):
+    if not os.path.exists(DB_DIR + '/' + sequenceId + '.json'):
         return 'requested sequenceId does not exist', 404
 
     # Retrieve the original request
-    with open('./db/' + sequenceId + '.json', 'r') as f:
+    with open(DB_DIR + '/' + sequenceId + '.json', 'r') as f:
         response = json.load(f)
     return response
 
 ################################################################################
 # RUN
 ################################################################################
+
 if __name__ == "__main__":
     application.debug = False
     application.run()
